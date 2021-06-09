738948c2dc549cd1ede21782169942c9b46f5d9e
Youngerjesus <yjh930919@gmail.com>
2021-06-09 13:03:50 +0900

    initialize

---
+++ src/main/java/com/study/querydsl/QuerydslApplication.java
@@ -0,0 +1,13 @@
+package com.study.querydsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuerydslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuerydslApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/querydsl/QuerydslApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.querydsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuerydslApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
